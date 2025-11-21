73de983b11a1602b03238155ea5aeb4a16f5d07e
unknown <tommynam00@gmail.com>
2025-11-21 15:56:56 -0500

    Functional

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 const display = document.getElementById("display");
+let justCalculated = false;
 
-function appendToDisplay(input){
-    display.value += input;
+function appendToDisplay(input) {
+  if (justCalculated) {
+    display.value = input; // replace display with new number
+    justCalculated = false;
+  } else {
+    display.value += input; // append normally
+  }
 }
 
-function backspace(){
-
+function backspace() {
+  let current = display.value;
+  display.value = current.slice(0, -1);
 }
 
-function oddEven(){
-
+function clearEntry() {
+  display.value = "";
 }
 
-function clearEntry(){
+function oddEven() {
+  let current = display.value;
+  if (current === "" || current === "0") {
+    return; // dont toggle if dispaly is empty or 0
+  }
 
+  if (current.startsWith('-')){
+    display.value = current.slice(1); // remove negative
+  } else {
+    display.value = '-' + current; // Add negative
+  }
 }
 
 function calculate(){
-
+    try{
+        display.value = eval(display.value);
+        justCalculated = true; // sets flag after calculating
+    }
+    catch(error){
+        display.value = "Error";
+        justCalculated = true;
+    }
 }
 
-function powerButton(){
-
-}
+function powerButton() {}
