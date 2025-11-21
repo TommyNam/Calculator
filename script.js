52e2061a6312ce4f5230c5181402fd033db47c8b
unknown <tommynam00@gmail.com>
2025-11-21 15:28:32 -0500

    Fixed up

---
+++ script.js
@@ -0,0 +1,25 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function backspace(){
+
+}
+
+function oddEven(){
+
+}
+
+function clearEntry(){
+
+}
+
+function calculate(){
+
+}
+
+function powerButton(){
+
+}
